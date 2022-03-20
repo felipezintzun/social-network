206e01c858d756e2a783d5c2d01d287dc0d30bf8
felipezintzun <felipezintzun@gmail.com>
2022-03-20 17:23:20 -0500

    correct user model

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,62 +1,65 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const ReplySchema = new Schema(
+const ReactionSchema = new Schema(
   {
-    // set custom id to avoid confusion with parent comment _id
-    replyId: {
+    reactionId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId()
     },
-    replyBody: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    writtenBy: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: createdAtVal => dateFormat(createdAtVal)
-    }
-  },
-  {
-    toJSON: {
-      getters: true
-    }
-  }
-);
-
-const ThoughtsSchema = new Schema(
-  {
-    writtenBy: {
+    reactionBody: {
       type: String
     },
-    thoughtBody: {
-      type: String
+    userName: {
+      type: String,
+      require: true
     },
     createdAt: {
       type: Date,
       default: Date.now,
       get: createdAtVal => dateFormat(createdAtVal)
     },
-    // use ReplySchema to validate data for a reply
-    replies: [ReplySchema]
   },
   {
     toJSON: {
-      virtuals: true,
       getters: true
     },
     id: false
   }
 );
 
-ThoughtsSchema.virtual('replyCount').get(function() {
-  return this.replies.length;
+const ThoughtsSchema = new Schema(
+  {
+    reactions: [ReactionSchema],
+    // set custom id to avoid confusion with parent comment _id
+    thoughtText: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  },
+
+);
+
+ThoughtsSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
 });
 
 const Thoughts = model('Thoughts', ThoughtsSchema);
